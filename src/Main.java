dce2605c51c731d11a4f5b5e798260292fad5cb5
Gregory <zmey106@yandex.ru>
2023-04-17 21:06:51 +0300

    JavaDZ_2.1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 19_231;
+        int mile = 20;
+
+
+        int bonusMiles = ticket / mile;
+
+
+        System.out.println("Количество бонусных миль: " + bonusMiles);
+    }
+}
